7e7699c4643606e86441ccebdbb4b1131f93f669
vegadelalyra <lyradeperseo@gmail.com>
2023-09-19 22:49:37 -0500

    feat: create mail template and module

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,34 @@
+import { HandlebarsAdapter } from '@nestjs-modules/mailer'
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.example.com',
+        secure: false,
+        auth: {
+          user: 'user@example.com',
+          pass: 'topsecret',
+        },
+      },
+      defaults: {
+        from: '"No Reply" <noreply@example.com>',
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+    
+  ],
+  providers: [MailService],
+  exports: [MailService]
+})
+export class MailModule {}
